b060a349dc9c00b0b1d25f02c9cebe52e26382d9
steelbridge-io <chris@steelbridge.io>
2025-02-12 01:23:01 -0800

    Add image carousel to front page template
    Introduced a Bootstrap-based image carousel on the front page for improved visual presentation. This includes three slides with example images and navigation controls for better user interaction.

--- front-page.php
+++ front-page.php
@@ -2,7 +2,29 @@
 /**
  * Front Page - The Fly Shop 2025
  */
-get_header();
+get_header(); ?>
 
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="img-fluid object-fit-cover" src="https://images.unsplash.com/photo-1738444515789-0a1a4c4982dc?q=80&w=3131&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img  class="img-fluid object-fit-cover" src="https://plus.unsplash.com/premium_photo-1738856608580-23d0ab1632e4?q=80&w=3175&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img  class="img-fluid object-fit-cover" src="https://images.unsplash.com/photo-1738898178964-88696087d43b?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
+<?php
 get_footer();
