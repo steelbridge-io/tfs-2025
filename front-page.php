17013ccf2fb9e857d4e9a96a982720a7eeaf7b7e
steelbridge-io <chris@steelbridge.io>
2025-02-13 20:19:29 -0800

    Add styles for navbar, front-page components, and carousel.
    This commit introduces updated styling for the navbar, including hover and scroll effects. It also adds layout updates for front-page elements such as CTA and custom carousel components, with support for media queries and animations for better responsiveness and visual appeal.

--- front-page.php
+++ front-page.php
@@ -7,13 +7,13 @@ get_header(); ?>
 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="img-fluid object-fit-cover" src="https://images.unsplash.com/photo-1516655855035-d5215bcb5604?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+      <img class="img-fluid object-fit-cover" src="https://placehold.co/3000x2000?text=Slider One" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img  class="img-fluid object-fit-cover" src="https://images.unsplash.com/photo-1437482078695-73f5ca6c96e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+      <img  class="img-fluid object-fit-cover" src="https://placehold.co/3000x2000?text=Slider Two" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img  class="img-fluid object-fit-cover" src="https://images.unsplash.com/photo-1480241352829-e1573ff2414e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+      <img  class="img-fluid object-fit-cover" src="https://placehold.co/3000x2000?text=Slider Three" class="d-block w-100" alt="...">
     </div>
       <!-- Add Scrolly -->
       <div id="scrolly" class="scrolly"><i class="lni lni-arrow-downward"></i></div>
@@ -28,78 +28,129 @@ get_header(); ?>
   </button>
 </div>
 
-<!-- Static Text Overlay -->
 <div class="carousel-overlay" data-aos="fade-up" data-aos-delay="800">
     <h1>Fly Fishing</h1>
     <h2>Gear &middot; Travel &middot; Schools</h2>
     <a href="#find-out-more" class="cta-button">Find Out More</a>
 </div>
 
-<!--<section>
-<div class="container-fluid container-row">
-    <div class="container pt-5">
-        <h2>The Fly Shop - America's &#35;1 Fly Fishing Retailer &amp; Specialists</h2>
-        <p class="lead">
-            Located in Redding, California, we offer premium fly fishing gear, local and global fly fishing travel to the worlds most idylic destinations and resorts. we offer private waters access throughout N. California for fly fishing anglers of all levels. Whether you're gearing for a weekend on our local waters or planning your next extended fly fishing adventure, we’ve got you covered.
-        </p>
+<section id="front-page-into">
+    <div class="container container-row front-page-into-container mt-5 mb-5">
+        <h1><span id="typing-area"></h1>
     </div>
+</section>
 
-</div>
-</section>-->
 <section class="content-section">
 <div id="front-page-content" class="container-fluid container-row">
-    <div class="container py-5">
-        <div class="row g-4">
-            <!-- Card 1 -->
-            <div class="col-md-6 card-feature top-card-feature-left" data-aos="fade-right" data-aos-offset="200" data-aos-duration="1000">
-                <div class="card-container">
-                    <img src="https://placehold.co/600x400" alt="Card 1" class="card-img">
-                    <div class="card-title-overlay">Card Title 1</div>
-                    <div class="card-hover-content">
-                        <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
-                        <a href="#" class="btn btn-primary btn-sm">Read More</a>
-                    </div>
+<div class="container">
+    <div class="row g-4">
+        <!-- Card 1 -->
+        <div class="col-md-6 card-feature top-card-feature-left" data-aos="fade-right" data-aos-offset="200" data-aos-duration="1000">
+            <div class="card-container">
+                <img src="https://placehold.co/600x400" alt="Card 1" class="card-img">
+                <div class="card-title-overlay">Card Title 1</div>
+                <div class="card-hover-content">
+                    <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Read More</a>
                 </div>
             </div>
+        </div>
 
-            <!-- Card 2 -->
-            <div class="col-md-6 card-feature top-card-feature-right" data-aos="fade-left" data-aos-offset="200" data-aos-duration="1000">
-                <div class="card-container">
-                    <img src="https://placehold.co/600x400" alt="Card 2" class="card-img">
-                    <div class="card-title-overlay">Card Title 2</div>
-                    <div class="card-hover-content">
-                        <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
-                        <a href="#" class="btn btn-primary btn-sm">Read More</a>
-                    </div>
+        <!-- Card 2 -->
+        <div class="col-md-6 card-feature top-card-feature-right" data-aos="fade-left" data-aos-offset="200" data-aos-duration="1000">
+            <div class="card-container">
+                <img src="https://placehold.co/600x400" alt="Card 2" class="card-img">
+                <div class="card-title-overlay">Card Title 2</div>
+                <div class="card-hover-content">
+                    <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Read More</a>
                 </div>
             </div>
+        </div>
 
-            <!-- Card 3 -->
-            <div class="col-md-6 card-feature bottom-card-feature-left" data-aos="fade-right" data-aos-offset="200" data-aos-duration="1000">
-                <div class="card-container">
-                    <img src="https://placehold.co/600x400" alt="Card 3" class="card-img">
-                    <div class="card-title-overlay">Card Title 3</div>
-                    <div class="card-hover-content">
-                        <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
-                        <a href="#" class="btn btn-primary btn-sm">Read More</a>
-                    </div>
+        <!-- Card 3 -->
+        <div class="col-md-6 card-feature bottom-card-feature-left" data-aos="fade-right" data-aos-offset="200" data-aos-duration="1000">
+            <div class="card-container">
+                <img src="https://placehold.co/600x400" alt="Card 3" class="card-img">
+                <div class="card-title-overlay">Card Title 3</div>
+                <div class="card-hover-content">
+                    <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Read More</a>
                 </div>
             </div>
+        </div>
 
-            <!-- Card 4 -->
-            <div class="col-md-6 card-feature bottom-card-feature-right" data-aos="fade-left" data-aos-offset="200" data-aos-duration="1000">
-                <div class="card-container">
-                    <img src="https://placehold.co/600x400" alt="Card 4" class="card-img">
-                    <div class="card-title-overlay">Card Title 4</div>
-                    <div class="card-hover-content">
-                        <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
-                        <a href="#" class="btn btn-primary btn-sm">Read More</a>
-                    </div>
+        <!-- Card 4 -->
+        <div class="col-md-6 card-feature bottom-card-feature-right" data-aos="fade-left" data-aos-offset="200" data-aos-duration="1000">
+            <div class="card-container">
+                <img src="https://placehold.co/600x400" alt="Card 4" class="card-img">
+                <div class="card-title-overlay">Card Title 4</div>
+                <div class="card-hover-content">
+                    <p class="lead">This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information. This is an excerpt of the card content. It slides up on hover to reveal additional information.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Read More</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</div>
+</section>
+
+<?php
+$carousel_items = get_option('fppc_carousel_items') ?: []; // Retrieve the carousel items
+if (!empty($carousel_items)): ?>
+    <section id="retail-front-page-carousel" class="custom-carousel-container">
+        <div id="productCarousel" class="custom-carousel">
+            <div class="carousel-inner-custom">
+						 <?php foreach ($carousel_items as $item): ?>
+                 <div class="carousel-item-custom">
+                     <div class="custom-card">
+                         <img src="<?php echo esc_url($item['image']); ?>" alt="<?php echo esc_attr($item['title']); ?>" class="custom-card-image" />
+                         <div class="custom-card-body">
+                             <h5 class="custom-card-title"><?php echo esc_html($item['title']); ?></h5>
+                             <p class="custom-card-text"><?php echo esc_html($item['description']); ?></p>
+                             <a href="<?php echo esc_url($item['url']); ?>" class="custom-card-link">View Product</a>
+                         </div>
+                     </div>
+                 </div>
+						 <?php endforeach; ?>
+            </div>
+
+            <!-- Navigation buttons -->
+            <button id="customPrev" class="custom-nav-btn custom-prev"><i class="bi bi-chevron-compact-left"></i>
+            </button>
+            <button id="customNext" class="custom-nav-btn custom-next"><i class="bi bi-chevron-compact-right"></i>
+            </button>
+        </div>
+    </section>
+<?php endif; ?>
+
+<section id="front-page-cta">
+    <div class="container-fluid container-row background-image-cta d-flex align-items-center mt-5">
+        <div class="container text-center text-md-end">
+            <div class="row justify-content-end">
+                <div class="col-md-6 col-lg-5 form-container shadow-lg p-5">
+                    <h5 class="mb-4 fw-bold">Stay Updated</h5>
+                    <p class="text-muted mb-4">Subscribe to our newsletter and never miss an update!</p>
+                    <form id="subscribe-form">
+                        <div class="form-floating mb-3">
+                            <input
+                                    type="email"
+                                    class="form-control shadow-sm"
+                                    id="subscriberEmail"
+                                    placeholder="name@example.com"
+                                    required
+                            />
+                            <label for="subscriberEmail" class="text-muted">Enter your email</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg px-4 shadow-sm">
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <?php
--- js/front-page.js
+++ js/front-page.js
@@ -28,3 +28,114 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+/** Typining Effect */
+const messages = [
+    "We are The Fly Shop. Located in Redding California. Shop with us for fly fishing gear, equipment, worldwide travel and...",
+    "the best guided fly fishing on the entire west coast."
+];
+
+const phoneNumber = "We'll get that fly rod bent proper: 800-669-3474"; // Final phone number to type
+const typingSpeed = 70; // Speed for typing each character
+const delayBetweenLines = 1000; // Delay before typing the next line
+const delayBeforeStart = 1500; // Delay before typing starts after scrolling into view
+const delayBeforeDelete = 1500; // Delay before deleting all text after lines are complete
+const typingTarget = document.getElementById("typing-area"); // Typing container
+const container = typingTarget.parentElement; // Parent container for typing area
+
+let messageIndex = 0; // Tracks which line/message is being typed
+let charIndex = 0; // Tracks the character being typed for current line
+let isDeleting = false; // Tracks if we are deleting text at the end
+let typingStarted = false; // Prevents re-triggering the typing effect
+
+// Dynamically adjust the container's height
+function updateContainerHeight() {
+    const tempDiv = document.createElement("div");
+    tempDiv.style.visibility = "hidden"; // Prevent it from affecting layout
+    tempDiv.style.position = "absolute";
+    tempDiv.style.whiteSpace = "pre-line"; // Matches typing style
+    tempDiv.innerHTML = typingTarget.innerHTML;
+
+    document.body.appendChild(tempDiv);
+    const requiredHeight = tempDiv.offsetHeight;
+    document.body.removeChild(tempDiv);
+
+    container.style.maxHeight = "none"; // Or set a higher limit like "500px"
+
+
+    container.style.height = `${requiredHeight}px`; // Update parent height
+    container.style.transition = "height 0.3s ease"; // Smooth transition
+}
+
+// Typing effect
+function typeEffect() {
+    if (!isDeleting) {
+        // Append characters to the typing target
+        let currentMessage = messages[messageIndex];
+        typingTarget.innerHTML = messages
+            .slice(0, messageIndex)
+            .join("<br>") + "<br>" + currentMessage.substring(0, charIndex);
+
+        updateContainerHeight(); // Adjust height for each character typed
+
+        charIndex++;
+
+        // If the entire line is typed, move to the next
+        if (charIndex > currentMessage.length) {
+            messageIndex++;
+            if (messageIndex >= messages.length) {
+                setTimeout(() => {
+                    isDeleting = true;
+                    typeEffect();
+                }, delayBeforeDelete);
+            } else {
+                charIndex = 0; // Reset for next message
+                setTimeout(typeEffect, delayBetweenLines);
+            }
+        } else {
+            setTimeout(typeEffect, typingSpeed);
+        }
+    } else {
+        // Deleting all text before typing the phone number
+        if (typingTarget.innerHTML.length > 0) {
+            typingTarget.innerHTML = typingTarget.innerHTML.slice(0, -1); // Remove one character
+            updateContainerHeight(); // Adjust height while deleting
+            setTimeout(typeEffect, typingSpeed / 2); // Deletion speed
+        } else {
+            // Reset character index and type the phone number
+            isDeleting = false;
+            charIndex = 0;
+            setTimeout(() => typePhoneNumber(), delayBetweenLines);
+        }
+    }
+}
+
+// Typing the phone number after all texts are deleted
+function typePhoneNumber() {
+    let currentText = typingTarget.innerHTML;
+    if (charIndex < phoneNumber.length) {
+        typingTarget.innerHTML = currentText + phoneNumber[charIndex];
+        updateContainerHeight(); // Adjust height for the phone number
+        charIndex++;
+        setTimeout(typePhoneNumber, typingSpeed);
+    }
+}
+
+// Intersection Observer to trigger typing effect when visible
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting && !typingStarted) {
+                typingStarted = true;
+                setTimeout(() => typeEffect(), delayBeforeStart); // Start typing after delay
+            }
+        });
+    },
+    {
+        threshold: 0.5 // Trigger when 50% of the container is visible
+    }
+);
+
+// Observe the container
+observer.observe(container);
