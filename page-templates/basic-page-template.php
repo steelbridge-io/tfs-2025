091bcf3cdee744c311a8520c9f0777d3010e1ff2
steelbridge-io <chris@steelbridge.io>
2025-04-06 17:01:49 -0700

    Add blog and page template files for The Fly Shop 2025
    Introduce several new template files, such as basic, blog, travel, and outfitters templates, to enhance the site's functionality and design capabilities. These templates support various post types and customizations, including featured images, videos, and dynamic content.

---
+++ page-templates/basic-page-template.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Template Name: Basic Template
+ * Template Post Type: post, page, travel_cpt, schools_cpt, adventures, guide_service, fishcamp_cpt, travel_blog, lower48, lower48blog
+ * Developed for The Fly Shop
+ * @package The_Fly_Shop
+ * Author: Chris Parsons
+ * Author URL: https://steelbridge.io
+ */
+
+$basic_the_post_img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');
+$the_post_default = get_bloginfo('template_directory') . '/images/default/default-page-header.png';
+$basic_logo_upload = get_theme_mod ('basic_page_logo');
+$hero_video_url = get_post_meta(get_the_ID(), 'hero-video-url', true );
+$publication_cta_img_1  =   get_post_meta(get_the_ID(), 'publication-cta-img-1', true );
+$publication_cta_img_2  =   get_post_meta(get_the_ID(), 'publication-cta-img-2', true );
+$publication_cta_img_3  =   get_post_meta(get_the_ID(), 'publication-cta-img-3', true );
+$publication_cta_img_4  =   get_post_meta(get_the_ID(), 'publication-cta-img-4', true );
+$default = '';
+
+get_header();
+
+include_once('post-meta/post-meta-basic.php');
+
+if (has_post_thumbnail()) :
+?>
+    <div class="container-fluid p-0">
+        <div class="hero-image position-relative">
+            <!-- Full-Width Featured Image -->
+            <img src="<?php echo esc_url(
+                    has_post_thumbnail() ?
+                        get_the_post_thumbnail_url(get_the_ID(), 'full') :
+                        get_template_directory_uri() . '/images/the-fly-shop-logo-white.png'
+                ); ?>"
+                 class="img-fluid w-100"
+                 alt="<?php the_title_attribute(); ?>">
+
+            <!-- Overlay Content -->
+            <div class="hero-overlay position-absolute top-50 start-50 translate-middle text-center">
+                <!-- Logo -->
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/images/the-fly-shop-logo-white.png'); ?>"
+                     class="hero-logo mb-3"
+                     alt="Website Logo">
+
+                <!-- Page Title -->
+                <h1 class="hero-title display-4 text-white"><?php echo get_the_title(); ?></h1>
+            </div>
+        </div>
+    </div>
+<?php else:  ?>
+    <div class="container-fluid p-0">
+        <div class="hero-image position-relative">
+            <!-- Full-Width Featured Image -->
+            <img src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2025/01/Staff_Main6.webp" class="img-fluid w-100" alt="<?php echo get_the_title(); ?>">
+            <!-- Overlay Content -->
+            <div class="hero-overlay position-absolute top-50 start-50 translate-middle text-center">
+                <!-- Logo -->
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/images/the-fly-shop-logo-white.png'); ?>"
+                     class="hero-logo mb-3"
+                     alt="Website Logo">
+
+                <!-- Page Title -->
+                <h1 class="hero-title display-4 text-white"><?php echo get_the_title(); ?></h1>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+
+    <div class="container-fluid top-fade p-0"></div>
+
+    <div class="container mt-6 mb-7">
+        <div class="container mt-4">
+				 <?php the_fly_shop_breadcrumbs(); ?>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <main id="primary" class="site-main">
+
+                    <?php
+                    while (have_posts()) :
+                    the_post();
+
+                    get_template_part('template-parts/content', 'page');
+
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if (comments_open() || get_comments_number()) :
+                    comments_template();
+                    endif;
+
+                    endwhile; // End of the loop.
+                    ?>
+
+                </main><!-- #main -->
+            </div>
+            <div class="col-md-4 page-sidebar">
+            <?php
+            get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
+<?php
+get_footer(); ?>
+
+
+ <script> var _ctct_m = "0a0f5b541f83f517b80813b9cfbdb8d9"; </script>
+ <script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
---
+++ page-templates/blog-template-basic.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Template Name: Blog Template Basic
+ * Template Post Type: post, page, travel_blog, lower48blog, fish_report, travel-blog, esb_lodge
+ * Developed for The Fly Shop
+ * @package The_Fly_Shop_2025
+ * Author: Chris Parsons
+ * Author URL: https://steelbridge.io
+ */
+
+get_header();
+
+if (has_post_thumbnail()) :
+ ?>
+ <div class="container-fluid p-0">
+	<div class="hero-image position-relative">
+	 <!-- Full-Width Featured Image -->
+	 <img src="<?php echo esc_url(
+		has_post_thumbnail() ?
+		 get_the_post_thumbnail_url(get_the_ID(), 'full') :
+		 get_template_directory_uri() . '/images/the-fly-shop-logo-white.png'
+	 ); ?>"
+				class="img-fluid w-100"
+				alt="<?php the_title_attribute(); ?>">
+
+	 <!-- Overlay Content -->
+	 <div class="hero-overlay position-absolute top-50 start-50 translate-middle text-center">
+		<!-- Logo -->
+		<img src="<?php echo esc_url(get_template_directory_uri() . '/images/the-fly-shop-logo-white.png'); ?>"
+				 class="hero-logo mb-3"
+				 alt="Website Logo">
+
+		<!-- Page Title -->
+		<h1 class="hero-title display-4 text-white"><?php echo get_the_title(); ?></h1>
+	 </div>
+	</div>
+ </div>
+<?php else:  ?>
+ <div class="container-fluid p-0">
+	<div class="hero-image position-relative">
+	 <!-- Full-Width Featured Image -->
+	 <img src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2025/01/Staff_Main6.webp" class="img-fluid w-100" alt="<?php echo get_the_title(); ?>">
+	 <!-- Overlay Content -->
+	 <div class="hero-overlay position-absolute top-50 start-50 translate-middle text-center">
+		<!-- Logo -->
+		<img src="<?php echo esc_url(get_template_directory_uri() . '/images/the-fly-shop-logo-white.png'); ?>"
+				 class="hero-logo mb-3"
+				 alt="Website Logo">
+
+		<!-- Page Title -->
+		<h1 class="hero-title display-4 text-white"><?php echo get_the_title(); ?></h1>
+	 </div>
+	</div>
+ </div>
+<?php endif; ?>
+
+ <div class="container-fluid top-fade p-0"></div>
+
+ <div class="container mt-6 mb-7">
+	<div class="container mt-4">
+	 <?php the_fly_shop_breadcrumbs(); ?>
+	</div>
+	<div class="row">
+	 <div class="col-md-8">
+		<main id="primary" class="site-main">
+
+		 <?php
+		 while (have_posts()) :
+			the_post();
+
+			//get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template-parts/content', 'basic' );
+
+			the_post_navigation(
+			 array(
+				'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'the-fly-shop-2025' ) . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'the-fly-shop-2025' ) . '</span> <span class="nav-title">%title</span>',
+			 )
+			);
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if (comments_open() || get_comments_number()) :
+			 comments_template();
+			endif;
+
+		 endwhile; // End of the loop.
+		 ?>
+
+		</main><!-- #main -->
+	 </div>
+	 <div class="col-md-4 page-sidebar">
+		<?php get_sidebar(); ?>
+	 </div>
+	</div>
+ </div>
+<?php
+get_footer();
