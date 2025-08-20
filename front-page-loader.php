6bda633f0f02ae54d794f4308353af2d8f5c24c2
steelbridge-io <chris@steelbridge.io>
2025-08-20 00:38:27 -0700

    Load `dashboard-customizer.php` in front-page loader for dashboard customization functionality.

--- front-page-loader.php
+++ front-page-loader.php
@@ -7,3 +7,6 @@ require_once get_template_directory() . '/front-page/front-page-ajax.php';
 
 // Then load settings page that might use those AJAX handlers
 require_once get_template_directory() . '/front-page/front-page-settings.php';
+
+// Load dashboard customizer
+require_once get_template_directory() . '/front-page/dashboard-customizer.php';
