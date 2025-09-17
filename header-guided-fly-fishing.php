a1741b886d8c92d44c68f571d63f5c3ee36000d1
steelbridge-io <chris@steelbridge.io>
2025-09-16 22:40:18 -0700

    Update `header-guided-fly-fishing.php` to use new `no-scroll` logo file path and add conditional below-navigation logo container in `the-fly-shop-2025` theme.

--- header-guided-fly-fishing.php
+++ header-guided-fly-fishing.php
@@ -53,7 +53,7 @@
           <!-- Brand/Logo (Centered) -->
           <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
            <img class="tfs-nav-logo scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2021/05/social_tfs_logo_og.png" alt="The Fly Shop 2025" />
-           <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2017/06/TFSLogo.png" alt="The Fly Shop 2025" />
+           <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2025/09/tfs-logo-600x484-1-1.png" alt="The Fly Shop 2025" />
           </a>
 
           <?php
@@ -74,6 +74,20 @@
          </div>
         </div>
        </nav>
+
+    <?php $dest_travel_logo = get_post_meta(get_the_ID(), 'guide-service-logo', true); ?>
+
+    <!-- Below navigation logo container - Shows initially, hides on scroll -->
+    <div id="below-nav-logo" class="below-nav-logo-container">
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+            <img class="tfs-nav-logo scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2021/05/social_tfs_logo_og.png" alt="The Fly Shop 2025" />
+            <?php if ($dest_travel_logo !== '') : ?>
+                <img class="tfs-nav-logo no-scroll mb-5" loading="eager" src="<?php echo $dest_travel_logo; ?>" alt="The Fly Shop 2025" />
+            <?php else: ?>
+            <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2025/09/tfs-logo-600x484-1-1.png" alt="The Fly Shop 2025" />
+        </a>
+        <?php endif; ?>
+    </div>
 
 
 	</header><!-- #masthead -->
