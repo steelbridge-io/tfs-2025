d78966398ab571303d73b7b286093926b5cf55ee
steelbridge-io <chris@steelbridge.io>
2025-09-26 00:27:47 -0700

    Update navigation logo links in `header.php` of `the-fly-shop-2025` theme to use placeholders instead of dynamic home URL.

--- header.php
+++ header.php
@@ -53,7 +53,7 @@
                     ?>
 
                     <!-- Brand/Logo (Centered) - Hidden initially, shows on scroll -->
-                    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+                    <a class="navbar-brand" href="#">
                         <img class="tfs-nav-logo scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2021/05/social_tfs_logo_og.png" alt="The Fly Shop 2025" />
                         <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2017/06/TFSLogo.png" alt="The Fly Shop 2025" />
                     </a>
@@ -79,7 +79,7 @@
 
         <!-- Below navigation logo container - Shows initially, hides on scroll -->
         <div id="below-nav-logo" class="below-nav-logo-container">
-            <a href="<?php echo esc_url(home_url('/')); ?>">
+            <a href="#">
                 <img class="tfs-nav-logo scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2021/05/social_tfs_logo_og.png" alt="The Fly Shop 2025" />
                 <img class="tfs-nav-logo no-scroll" loading="eager" src="https://tfs-spaces.sfo2.digitaloceanspaces.com/theflyshop/uploads/2025/09/tfs-logo-600x484-1-1.png" alt="The Fly Shop 2025" />
             </a>
